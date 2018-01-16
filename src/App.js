fadfb9ca873ec3ead4a5e65ac02a4a421289eef2
Doug Whitney <dwhitney@wirelessregistry.com>
2018-01-16 16:40:50 -0700

    Demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactMapGL from 'react-map-gl'
+import DeckGL, { LineLayer } from 'deck.gl'
+import drawPolygon from './polygon-drawing'
+import { cloneDeep as _cloneDeep } from 'lodash'
+import autobind from 'react-autobind'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    autobind(this)
+  }
 
   state = {
     viewport: {
       width: 1500,
       height: 750,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
+      latitude: 39.7392,
+      longitude: -104.9903,
+      zoom: 15
+    },
+    paths: []
+  }
+
+  updatePaths(lngLat, event) {
+    const paths = _cloneDeep(this.state.paths)
+
+    return drawPolygon(paths, lngLat, event === 'click')
+  }
+
+  onClick({ lngLat }) {
+    this.setState({ paths: this.updatePaths(lngLat, 'click') })
+  }
+
+  onHover({ lngLat }) {
+    const { paths } = this.state
+
+    paths.length && this.setState({ paths: this.updatePaths(lngLat) })
   }
 
   render() {
+    const { viewport, paths } = this.state
+    const layer = new LineLayer({
+      id: 'line-layer',
+      data: paths,
+      strokeWidth: 5,
+      getSourcePosition: ({ start }) => start,
+      getTargetPosition: ({ end }) => end,
+      getColor: () => [255, 0, 0, 100]
+    })
+
+    console.log(paths)
+
     return (
       <ReactMapGL
-        {...this.state.viewport}
+        {...viewport}
         onClick={this.onClick}
-        onViewportChange={(viewport) => this.setState({viewport})}
-      />
+        onHover={this.onHover}
+        onViewportChange={(viewport) => this.setState({viewport})}>
+
+        <DeckGL layers={[layer]} {...viewport} />
+
+      </ReactMapGL>
     )
   }
 }
 
 export default App
+
+
---
+++ src/polygon-drawing.js
@@ -0,0 +1,32 @@
+const drawPolygon = (paths, lngLat, setPoint) => {
+  const isFirstPoint = setPoint && !paths.length
+
+  return isFirstPoint ? startPaths(paths, lngLat) : endPath(paths, lngLat, setPoint)
+}
+
+const endPath = (paths, end, setPoint) => {
+  paths.forEach((path, index) => {
+    const isFirst = index === 0
+    const isLast = (index === paths.length - 1)
+
+    isFirst && (path.start = end)
+    isLast && (path.end = end)
+  })
+
+  return setPoint ? startPaths(paths, end) : paths
+}
+
+const startPaths = (paths, start) => {
+  const newPath = () => {
+    return {
+      color: [255,255,255,255],
+      end: start,
+      start
+    }
+  }
+  const newPaths = paths.length ? newPath() : [newPath(), newPath()]
+
+  return paths.concat(newPaths)
+}
+
+export default drawPolygon
